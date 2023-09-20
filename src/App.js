aa02a774a670ea15243eb237b0b43b467cf242b3
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-09-20 14:46:12 +0100

    created new Interval and add +100 to time every 100ms / used useEffect to clearInterval

--- src/App.js
+++ src/App.js
@@ -2,19 +2,56 @@ import './styles/global.scss';
 import './styles/typography.scss';
 import Timer from './components/Timer/Timer';
 import Button from './components/Button/Button';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+	const [time, setTime] = useState(0);
+	const [timer, setTimer] = useState(false);
+
+	useEffect(() => {
+		//code on start
+		if (timer) {
+			const intervalTimer = setInterval(() => {
+				setTime(prevTime => prevTime + 100);
+			}, 100);
+
+			return () => {
+				//code on the end
+				clearInterval(intervalTimer);
+			};
+		}
+	});
+
+	const handleStart = () => {
+		setTimer(true);
+	};
+
+	const handleStop = () => {
+		setTimer(false);
+	};
+
+	const handleReset = () => {
+		setTimer(false);
+		setTime(0);
+	};
+
 	return (
 		<div>
 			<h1 className="header1">Stoper App</h1>
-			<Timer />
+			<Timer time={time} />
 			<section className="center">
-				<Button type="button">Start</Button>
-				<Button type="button">Stop</Button>
-				<Button type="button">Reset</Button>
+				<Button type="button" onClick={handleStart}>
+					Start
+				</Button>
+				<Button type="button" onClick={handleStop}>
+					Stop
+				</Button>
+				<Button type="button" onClick={handleReset}>
+					Reset
+				</Button>
 			</section>
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,9 +1,6 @@
 import styles from './Timer.module.scss';
-import { useState } from 'react';
 
-const Timer = () => {
-	const [time, setTime] = useState('00:00:00.0');
-
+const Timer = ({ time }) => {
 	return <p className={styles.timer}>{time}</p>;
 };
 
