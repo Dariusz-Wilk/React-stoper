ffc1c7f788d1903ee714f98433a0ef3aaec52227
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-09-20 15:10:21 +0100

    display time in correct format

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,23 @@
 import styles from './Timer.module.scss';
 
 const Timer = ({ time }) => {
-	return <p className={styles.timer}>{time}</p>;
+	Math.floor();
+	const hours = Math.floor((time / 1000 / 60 / 60) % 60)
+		.toString()
+		.padStart(2, '0');
+	const mins = Math.floor((time / 1000 / 60) % 60)
+		.toString()
+		.padStart(2, '0');
+	const secs = Math.floor((time / 1000) % 60)
+		.toString()
+		.padStart(2, '0');
+	const msec = Math.floor((time / 100) % 10);
+
+	return (
+		<p className={styles.timer}>
+			{hours}:{mins}:{secs}.{msec}
+		</p>
+	);
 };
 
 export default Timer;
