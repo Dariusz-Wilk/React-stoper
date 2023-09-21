894a0342d74c0d9c813780170572a1815e7a0486
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-09-21 01:45:23 +0100

    coded stop-watch vervion with no useEffect hook

--- src/App.js
+++ src/App.js
@@ -2,38 +2,75 @@ import './styles/global.scss';
 import './styles/typography.scss';
 import Timer from './components/Timer/Timer';
 import Button from './components/Button/Button';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+// import { useEffect } from 'react';
 
 const App = () => {
 	const [time, setTime] = useState(0);
-	const [timer, setTimer] = useState(false);
 
-	useEffect(() => {
-		let intervalTimer;
-		//code on start
-		if (timer) {
-			intervalTimer = setInterval(() => {
-				setTime(prevTime => prevTime + 100);
-			}, 100);
-		}
-		return () => {
-			//code on the end component live
-			if (intervalTimer) clearInterval(intervalTimer);
-		};
-	}, [timer]);
+	// ========================================
+	// ===== ver. with no UseEffect ===========
+	// ========================================
+
+	const [timer, setTimer] = useState(setInterval(() => {}, 100));
+	const [isRunning, setIsRunning] = useState(false);
 
 	const handleStart = () => {
-		setTimer(true);
+		if (!isRunning) {
+			setIsRunning(true);
+			setTimer(
+				setInterval(() => {
+					setTime(prevTime => prevTime + 100);
+				}, 100)
+			);
+		}
 	};
 
 	const handleStop = () => {
-		setTimer(false);
+		clearInterval(timer);
+		setIsRunning(false);
 	};
 
 	const handleReset = () => {
-		setTimer(false);
+		clearInterval(timer);
 		setTime(0);
+		setIsRunning(false);
 	};
+
+	// =========================================
+	//========== USEEFFECT version =============
+	//==========================================
+
+	// const [timer, setTimer] = useState(false);
+
+	// useEffect(() => {
+	// 	let intervalTimer;
+	// 	//code on start
+	// 	if (timer) {
+	// 		intervalTimer = setInterval(() => {
+	// 			setTime(prevTime => prevTime + 100);
+	// 		}, 100);
+	// 	}
+	// 	return () => {
+	// 		//code on the end component live
+	// 		if (intervalTimer) {
+	// 			clearInterval(intervalTimer);
+	// 		}
+	// 	};
+	// }, [timer]);
+
+	// const handleStart = () => {
+	// 	setTimer(true);
+	// };
+
+	// const handleStop = () => {
+	// 	setTimer(false);
+	// };
+
+	// const handleReset = () => {
+	// 	setTimer(false);
+	// 	setTime(0);
+	// };
 
 	return (
 		<div>
